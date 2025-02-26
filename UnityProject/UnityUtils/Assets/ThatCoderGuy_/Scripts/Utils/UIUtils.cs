4f696700ed445bd366a1415bbffb160fca208f28
Andrew Maclean <andrew.maclean@hyperluminalgames.com>
2025-02-26 10:13:10 +0000

    Adding UIUtils script

---
+++ UnityProject/UnityUtils/Assets/ThatCoderGuy_/Scripts/Utils/UIUtils.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TCG.Utils
+{
+    public static class UIUtils
+    {
+
+        /// <summary>
+        /// Go through all the child transforms of an object and call ForceRebuildLayoutImmediate on any RectTransform components found
+        /// </summary>
+        /// <typeparam name="T">Type of the singleton</typeparam>
+        public void ForceRebuildFromChildBackwards(this Transform transformToRebuild )
+        {
+            foreach ( Transform child in transformToRebuild )
+            {
+                ForceRebuildFromChildBackwards( child );
+            }
+            RectTransform rectTransform = transformToRebuild as RectTransform;
+            if ( rectTransform != null )
+            {
+                LayoutRebuilder.ForceRebuildLayoutImmediate( rectTransform );
+            }
+        }        
+    }
+}
